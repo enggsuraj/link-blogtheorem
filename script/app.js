e494a020af1350dcd50b9191c564d5fdf26355eb
Suraj <info.blogtheorem@gmail.com>
2021-02-03 02:42:36 +0530

    v1.0

--- script/app.js
+++ script/app.js
@@ -20,3 +20,34 @@ AOS.init({
   mirror: false,
   anchorPlacement: "top-bottom",
 });
+
+// CLCIK AND DRAG
+const slider = document.querySelector(".wrapper");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
